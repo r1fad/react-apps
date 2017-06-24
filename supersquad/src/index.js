4b986cf8e15e967d5e08ecd0bd80e9684e2e22bd
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-06-24 15:33:33 +0300

    completed SuperSquad

---
+++ supersquad/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+
+
+const store = createStore(rootReducer);
+console.log(store.getState());
+store.subscribe(( () => console.log('store', store.getState())));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
