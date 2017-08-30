2941c48034f2f9533f184f9d6a792f9189241c01
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-08-30 15:14:37 +0300

    display memes

---
+++ generator/src/components/MemeItem.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class MemeItem extends Component{
+  constructor(){
+    super();
+
+    this.state={
+      hovered: false
+    }
+  }
+
+  render(){
+    return(
+      <div className="meme-item"
+           onMouseEnter={()=> this.setState({hovered:true})}
+           onMouseLeave={()=> this.setState({hovered:false})}>
+        <img
+          className={this.state.hovered ? "meme-image darken-image" : "meme-image"}
+          src={this.props.meme.url}
+          alt={this.props.meme.name}/>
+        <p className={this.state.hovered ? "meme-text" : "no-text"}>
+          {this.props.meme.name}
+        </p>
+      </div>
+    )
+  }
+}
+
+export default MemeItem;
