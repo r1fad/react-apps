2cae64363297ffa768b63b8c75e199ee6f7bde32
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-08-30 13:43:41 +0300

    implement actions and reducers

--- generator/src/actions/index.js
+++ generator/src/actions/index.js
@@ -1,9 +1,26 @@
 export const RECEIVE_MEMES = 'RECEIVE_MEMES';
 
+/* action creator, return an object with action type
+and information*/
 function receiveMemes(json){
   const {memes} = json.data;
   return{
     type:RECEIVE_MEMES,
     memes
   }
-} 
+}
+
+/* performs a fetch on the Memes api and returns
+the response */
+function fetchMemesJson(){
+  return fetch('https://api.imgflip.com/get_memes')
+    .then(response => response.json())
+}
+
+/*returns a function */
+export function fetchMemes(){
+  return function(dispatch){
+    return fetchMemesJson().
+      .then(json => dispatch(receiveMemes(json)))
+  }
+}
